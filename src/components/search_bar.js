e0ba30a3647abc3459ddec104b0c8ba1975927c6
emilj858 <teamgoout@gmail.com>
2018-03-26 18:31:08 -0700

    added videos to list and selecting video to watch

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,7 @@ class SearchBar extends Component {
                 <input
                     value={this.state.term}
                     onChange={event => this.setState({ term: event.target.value })} />
-                Value of input: {this.state.term}
+                {/*Value of input: {this.state.term}*/}
             </div>
         )
     }
